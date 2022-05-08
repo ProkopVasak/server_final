76c33eb28e6963cf37d3360fd950a82be1d2726b
ProkopVasak <91657857+ProkopVasak@users.noreply.github.com>
2022-05-08 16:26:17 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,75 @@
+pins.touch_set_mode(TouchTarget.P1, TouchTargetMode.CAPACITIVE)
+pins.touch_set_mode(TouchTarget.P2, TouchTargetMode.CAPACITIVE)
+radio.set_group(69)
+radio.set_transmit_serial_number(True)
+radio.set_transmit_power(7)
+enabled = False
+list_cisla = [control.device_serial_number()]
+hlasy = [0]
+pouzite_cisla = [control.device_serial_number()]
+hlas = None
+a = 0
+b = 0
+c = 0
+d = 0
+counter = 0
+seriove_cislo = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+
+def on_button_pressed_b():
+    global enabled
+    if enabled == False:
+        radio.send_value("enabled", 1)
+        enabled = True
+    elif enabled == True:
+        radio.send_value("enabled", 0)
+        enabled = False
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_received_value(name, value):
+    global list_cisla, hlasy, seriove_cislo
+    if name == "vote":
+        radio.send_value("ack", seriove_cislo)
+        print(seriove_cislo)
+        list_cisla.append(seriove_cislo)
+        if value == 1:
+            hlasy.append(1)
+        if value == 2:
+            hlasy.append(2)
+        if value == 3:
+            hlasy.append(3)
+        if value == 4:
+            hlasy.append(4)
+radio.on_received_value(on_received_value)
+
+def vysledky():
+    global a, b, c, d, list_cisla, hlasy, pouzite_cisla, counter
+    for i in list_cisla:
+        while len(list_cisla) != len(list_cisla): 
+            if list_cisla[i] != pouzite_cisla[counter]:
+                counter += 1
+            else: break    
+        for g in list_cisla:
+            pouzite_cisla.append(list_cisla[i])    
+            if list_cisla[i] == list_cisla[g]:
+                hlas2 = hlasy[g]
+        hlasy.append(hlas2)
+        counter = 0
+    a = hlasy.count(1)
+    b = hlasy.count(2)
+    c = hlasy.count(3)
+    d = hlasy.count(4)
+    print("hlasy A = " + a)
+    print("hlasy B = " + b)
+    print("hlasy C = " + c)
+    print("hlasy D = " + d)
+input.on_button_pressed(Button.A, vysledky)
+
+def reset():
+    global a, b, c, d, hlasy
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    seriova_cisla = []
+    hlasy = []
+input.on_button_pressed(Button.AB, reset)
---
+++ main.ts
@@ -0,0 +1,92 @@
+pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
+radio.setGroup(69)
+radio.setTransmitSerialNumber(true)
+radio.setTransmitPower(7)
+let enabled = false
+let list_cisla = [control.deviceSerialNumber()]
+let hlasy = [0]
+let pouzite_cisla = [control.deviceSerialNumber()]
+let hlas = null
+let a = 0
+let b = 0
+let c = 0
+let d = 0
+let counter = 0
+let seriove_cislo = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (enabled == false) {
+        radio.sendValue("enabled", 1)
+        enabled = true
+    } else if (enabled == true) {
+        radio.sendValue("enabled", 0)
+        enabled = false
+    }
+    
+})
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    
+    if (name == "vote") {
+        radio.sendValue("ack", seriove_cislo)
+        console.log(seriove_cislo)
+        list_cisla.push(seriove_cislo)
+        if (value == 1) {
+            hlasy.push(1)
+        }
+        
+        if (value == 2) {
+            hlasy.push(2)
+        }
+        
+        if (value == 3) {
+            hlasy.push(3)
+        }
+        
+        if (value == 4) {
+            hlasy.push(4)
+        }
+        
+    }
+    
+})
+input.onButtonPressed(Button.A, function vysledky() {
+    let hlas2: number;
+    
+    for (let i of list_cisla) {
+        while (list_cisla.length != list_cisla.length) {
+            if (list_cisla[i] != pouzite_cisla[counter]) {
+                counter += 1
+            } else {
+                break
+            }
+            
+        }
+        for (let g of list_cisla) {
+            pouzite_cisla.push(list_cisla[i])
+            if (list_cisla[i] == list_cisla[g]) {
+                hlas2 = hlasy[g]
+            }
+            
+        }
+        hlasy.push(hlas2)
+        counter = 0
+    }
+    a = _py.py_array_count(hlasy, 1)
+    b = _py.py_array_count(hlasy, 2)
+    c = _py.py_array_count(hlasy, 3)
+    d = _py.py_array_count(hlasy, 4)
+    console.log("hlasy A = " + a)
+    console.log("hlasy B = " + b)
+    console.log("hlasy C = " + c)
+    console.log("hlasy D = " + d)
+})
+input.onButtonPressed(Button.AB, function reset() {
+    
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    let seriova_cisla = []
+    hlasy = []
+})
